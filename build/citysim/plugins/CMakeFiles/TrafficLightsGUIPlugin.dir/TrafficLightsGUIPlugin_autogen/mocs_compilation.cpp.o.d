8d79341c5c22426a84cbbf2aa9afac5ba2041920
thomas <vinesthomas@gmail.com>
2025-08-25 16:26:13 -0400

    Add build and install to .gitignore

--- build/citysim/plugins/CMakeFiles/TrafficLightsGUIPlugin.dir/TrafficLightsGUIPlugin_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-plugins/CMakeFiles/TrafficLightsGUIPlugin.dir/TrafficLightsGUIPlugin_autogen/mocs_compilation.cpp.o: \
- /home/minnie/gazebo_sim/citysim/build/citysim/plugins/TrafficLightsGUIPlugin_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/citysim/plugins/CMakeFiles/TrafficLightsGUIPlugin_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/TrafficLightsGUIPlugin_autogen"
-  "TrafficLightsGUIPlugin_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TrafficLightsGUIPlugin_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
