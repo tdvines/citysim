e3bf46bc32bcdb234596534e1da047af9ce886a4
thomas <vinesthomas@gmail.com>
2025-08-25 16:06:30 -0400

    Remove build and install directories from repo and add to .gitignore

--- build/citysim/plugins/CMakeFiles/BloomVisualPlugin.dir/BloomVisualPlugin_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-plugins/CMakeFiles/BloomVisualPlugin.dir/BloomVisualPlugin_autogen/mocs_compilation.cpp.o: \
- /home/minnie/gazebo_sim/citysim/build/citysim/plugins/BloomVisualPlugin_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
